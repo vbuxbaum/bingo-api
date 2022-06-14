578b5eae79a2ad4262a3f499a93b1106036fc95d
Vitor Buxbaum <vitor.buxbaum@gmail.com>
2022-06-14 16:51:31 -0300

    jk classic: assert wildcard is none

--- tests/test_bingo.py
+++ tests/test_bingo.py
@@ -28,3 +28,12 @@ def test_get_card_unique_values():
             card_values.add(value)
 
     assert len(card_values) == len(response.json()["card_columns"]) ** 2
+
+
+def test_get_card_wild_card():
+    response = client.get("/card")
+    assert response.status_code == 200
+    assert response.json()["card_type"] == "jk_classic"
+    card_columns = response.json()["card_columns"]
+
+    assert card_columns[len(card_columns) // 2][len(card_columns) // 2] is None
