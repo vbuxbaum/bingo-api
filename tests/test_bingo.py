7dbd7b8cd5855cae2c4db0c96c72e7ec196a5b93
Vitor Buxbaum <vitor.buxbaum@gmail.com>
2022-06-14 18:50:08 -0300

    using pytest fixtures

--- tests/test_bingo.py
+++ tests/test_bingo.py
@@ -1,11 +1,14 @@
 from fastapi.testclient import TestClient
+import pytest
 from main import app
 
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
 
-def test_home():
+def test_home(client):
     res = client.get("/")
     assert res.status_code == 200
     assert (
@@ -13,40 +16,40 @@ def test_home():
     )
 
 
-def test_get_default_card():
-    response = client.get("/card")
-    assert response.status_code == 200
-    assert response.json()["card_type"] == "jk_classic"
-    assert len(response.json()["card_columns"]) == 5
+def test_get_default_card(client):
+    res = client.get("/card")
+    assert res.status_code == 200
+    assert res.json()["card_type"] == "jk_classic"
+    assert len(res.json()["card_columns"]) == 5
 
 
-def test_get_card_valid_type():
-    response = client.get("/card?card_type=jk_classic")
-    assert response.status_code == 200
-    assert response.json()["card_type"] == "jk_classic"
+def test_get_card_valid_type(client):
+    res = client.get("/card?card_type=jk_classic")
+    assert res.status_code == 200
+    assert res.json()["card_type"] == "jk_classic"
 
 
-def test_get_card_invalid_type():
-    response = client.get("/card?card_type=invalid_type")
-    assert response.status_code == 404
-    err_msg = response.json()["detail"]
+def test_get_card_invalid_type(client):
+    res = client.get("/card?card_type=invalid_type")
+    assert res.status_code == 404
+    err_msg = res.json()["detail"]
     assert err_msg == "The 'invalid_type' card type is unknown."
 
 
-def test_get_card_unique_values():
-    response = client.get("/card")
-    assert response.status_code == 200
+def test_get_card_unique_values(client):
+    res = client.get("/card")
+    assert res.status_code == 200
     card_values = set()
-    for columns in response.json()["card_columns"]:
+    for columns in res.json()["card_columns"]:
         for value in columns:
             card_values.add(value)
 
-    assert len(card_values) == len(response.json()["card_columns"]) ** 2
+    assert len(card_values) == len(res.json()["card_columns"]) ** 2
 
 
-def test_get_card_wild_card():
-    response = client.get("/card")
-    assert response.status_code == 200
-    card_columns = response.json()["card_columns"]
+def test_get_card_wild_card(client):
+    res = client.get("/card")
+    assert res.status_code == 200
+    card_columns = res.json()["card_columns"]
 
     assert card_columns[len(card_columns) // 2][len(card_columns) // 2] is None
