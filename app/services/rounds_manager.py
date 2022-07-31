976bdaf9e731f981ae7a131f7a4ef498929d0fee
Vitor Buxbaum <vitor.buxbaum@gmail.com>
2022-07-31 17:10:28 -0300

    service for picking number for round

--- app/services/rounds_manager.py
+++ app/services/rounds_manager.py
@@ -1,18 +1,23 @@
+from pymongo import ReturnDocument
 from app.models.base_model import db
 from fastapi.encoders import jsonable_encoder
 from pymongo.results import DeleteResult
+
+from app.models.round_model import RoundModel
 
 
 class RoundManager:
     rounds_collection = db["rounds"]
 
     @staticmethod
-    async def create(round):
+    async def create(round: RoundModel):
+        round.numbers_to_pick = list(range(1, 76))
         round = jsonable_encoder(round)
         new_round = await db["rounds"].insert_one(round)
         created_round = await db["rounds"].find_one(
             {"_id": new_round.inserted_id}
         )
+        del round
         return created_round
 
     @staticmethod
@@ -22,6 +27,25 @@ class RoundManager:
     @staticmethod
     async def get_one_by_id(id):
         return await db["rounds"].find_one({"_id": id})
+
+    @staticmethod
+    async def pick_number(id):
+        found_round = await RoundManager.get_one_by_id(id)
+        if not found_round:
+            return None
+        if found_round["is_round_over"]:
+            return found_round
+
+        round = RoundModel(**found_round)
+        round.pick_number()
+
+        updated_round = await db["rounds"].find_one_and_replace(
+            {"_id": id},
+            jsonable_encoder(round),
+            return_document=ReturnDocument.AFTER,
+        )
+        del round
+        return updated_round
 
     @staticmethod
     async def delete_one(id: str) -> bool:
