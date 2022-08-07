a8c85e7dc69468b3f620dac8fd1765ff498c95ed
Vitor Buxbaum <vitor.buxbaum@gmail.com>
2022-08-07 19:42:06 -0300

    refact RoundsManager: staticmethod > classmethod

--- app/services/rounds_manager.py
+++ app/services/rounds_manager.py
@@ -7,30 +7,30 @@ from app.models.round_model import RoundModel
 
 
 class RoundManager:
-    rounds_collection = db["rounds"]
+    db_collection = db["rounds"]
 
-    @staticmethod
-    async def create(round: RoundModel):
+    @classmethod
+    async def create(cls, round: RoundModel):
         round.numbers_to_pick = list(range(1, 76))
         round = jsonable_encoder(round)
-        new_round = await db["rounds"].insert_one(round)
-        created_round = await db["rounds"].find_one(
+        new_round = await cls.db_collection.insert_one(round)
+        created_round = await cls.db_collection.find_one(
             {"_id": new_round.inserted_id}
         )
         del round
         return created_round
 
-    @staticmethod
-    async def get_many():
-        return await db["rounds"].find().to_list(1000)
+    @classmethod
+    async def get_many(cls):
+        return await cls.db_collection.find().to_list(1000)
 
-    @staticmethod
-    async def get_one_by_id(id):
-        return await db["rounds"].find_one({"_id": id})
+    @classmethod
+    async def get_one_by_id(cls, id):
+        return await cls.db_collection.find_one({"_id": id})
 
-    @staticmethod
-    async def pick_number(id):
-        found_round = await RoundManager.get_one_by_id(id)
+    @classmethod
+    async def pick_number(cls, id):
+        found_round = await cls.get_one_by_id(id)
         if not found_round:
             return None
         if found_round["is_round_over"]:
@@ -39,7 +39,7 @@ class RoundManager:
         round = RoundModel(**found_round)
         round.pick_number()
 
-        updated_round = await db["rounds"].find_one_and_replace(
+        updated_round = await cls.db_collection.find_one_and_replace(
             {"_id": id},
             jsonable_encoder(round),
             return_document=ReturnDocument.AFTER,
@@ -47,9 +47,9 @@ class RoundManager:
         del round
         return updated_round
 
-    @staticmethod
-    async def delete_one(id: str) -> bool:
-        delete_result: DeleteResult = await db["rounds"].delete_one(
+    @classmethod
+    async def delete_one(cls, id: str) -> bool:
+        delete_result: DeleteResult = await cls.db_collection.delete_one(
             {"_id": id}
         )
         return bool(delete_result.deleted_count)
