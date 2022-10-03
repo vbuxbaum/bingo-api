9a89d43fe6347638eb19e9318a2e4ea82b61fae5
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-10-03 16:09:40 -0300

    test classic cards reasonably unique

--- tests/test_cards_model.py
+++ tests/test_cards_model.py
@@ -8,9 +8,18 @@ def test_compare_equal_cards():
     assert card_one == card_two
 
 
-def test_cards_hash():
-
+def test_card_is_hashable():
     card_one = card_generators.ClassicGenerator.generate_card()
     card_two = card_model.BingoCard(**card_one.dict())
-    card_three = card_generators.ClassicGenerator.generate_card()
-    assert len({card_one, card_two, card_three}) == 2
+    assert set([card_one, card_two])
+
+
+def test_reasonable_unique_cards():
+    test_size = 100
+    creation_count = 0
+    card_set = set()
+    while len(card_set) < test_size:
+        card_set.add(card_generators.ClassicGenerator.generate_card())
+        creation_count += 1
+
+    assert test_size / creation_count > 0.98
