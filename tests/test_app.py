0874f4b16298a21a4ef31800a08b5168fb3bb81d
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-10-03 23:48:08 -0300

    delete dummy test file

--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-def test_home(client):
-    res = client.get("/")
-    assert res.status_code == 200
-    assert (
-        "Let's play! Visit route /card for a random classic card."
-        in res.json()["message"]
-    )
