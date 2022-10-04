c2b34f13b8ff129c253acc84ada905b23123e4a9
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-10-03 23:57:26 -0300

    adds type annotation to fixture

--- tests/test_cards_route.py
+++ tests/test_cards_route.py
@@ -1,9 +1,10 @@
 from hypothesis import given
 import hypothesis.strategies as st
 from app.services.card_generators import CARD_GENERATORS
+from fastapi.testclient import TestClient
 
 
-def test_get_default_card(client):
+def test_get_default_card(client: TestClient):
     res = client.get("/card")
     assert res.status_code == 200
     assert res.json()["card_type"] == "classic"
@@ -11,13 +12,13 @@ def test_get_default_card(client):
 
 
 @given(st.sampled_from(list(CARD_GENERATORS)))
-def test_get_card_of_valid_type(client, card_type):
+def test_get_card_of_valid_type(client: TestClient, card_type):
     res = client.get(f"/card?card_type={card_type}")
     assert res.status_code == 200
     assert res.json()["card_type"] == card_type
 
 
-def test_get_card_invalid_type(client):
+def test_get_card_invalid_type(client: TestClient):
     res = client.get("/card?card_type=invalid_type")
     assert res.status_code == 404
 
@@ -25,14 +26,14 @@ def test_get_card_invalid_type(client):
     assert err_msg == "The 'invalid_type' card type is unknown."
 
 
-def test_get_n_default_columns_card(client):
+def test_get_n_default_columns_card(client: TestClient):
     res = client.get("/card?card_type=n_square")
     assert res.status_code == 200
     assert res.json()["card_type"] == "n_square"
     assert len(res.json()["card_values"]) == 5
 
 
-def test_get_n_card_invalid_card_size(client):
+def test_get_n_card_invalid_card_size(client: TestClient):
     res = client.get("/card?card_type=n_square&n=1")
     assert res.status_code == 422
 
