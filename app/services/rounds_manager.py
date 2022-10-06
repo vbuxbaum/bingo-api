15b11b538f5b909c88b51606a62044a3cd549b17
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-10-06 19:09:08 -0300

    refact: remove unnecessary contidional

--- app/services/rounds_manager.py
+++ app/services/rounds_manager.py
@@ -38,8 +38,6 @@ class RoundManager:
         found_round = cls.get_one_by_pin(pin)
         if not found_round:
             return None
-        if found_round["is_round_over"]:
-            return found_round
 
         round_to_join = RoundModel(**found_round)
 
@@ -71,8 +69,6 @@ class RoundManager:
         found_round = cls.get_one_by_id(id)
         if not found_round:
             return None
-        if found_round["is_round_over"]:
-            return found_round
 
         round = RoundModel(**found_round)
         round.pick_number()
